ee5694e888eb56e16969505b2dac83eaa41e56a9
Nicholas Hemerling <nicholas.hemerling@gmail.com>
2020-08-21 18:19:23 -0700

    make images links and change nav to use NavLink and activeClassName

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -1,16 +1,50 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './Nav.css';
 
 export default function Nav(props) {
     return(
         <nav>
             <ul>
-                <li><Link to='/'>Home</Link></li>
-                <li><Link to='/about'>About</Link></li>
-                <li><Link to='/portfolio'>Portfolio</Link></li>
-                <li><Link to='/contact'>Contact</Link></li>
+                <li>
+                    <NavLink
+                        exact
+                        activeClassName="navbar__link--active"
+                        className="navbar__link" 
+                        to='/'
+                    >
+                        Home
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        activeClassName="navbar__link--active"
+                        className="navbar__link"
+                        to='/about'
+                    >
+                        About
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        activeClassName="navbar__link--active"
+                        className="navbar__link"
+                        to='/portfolio'
+                    >
+                        Portfolio
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        activeClassName="navbar__link--active"
+                        className="navbar__link"
+                        to='/contact'
+                    >
+                        Contact
+                    </NavLink>
+                </li>
             </ul>
         </nav>
     );
 }
+// YOURE GOING TO MAKE THESE NAV LINKS AND DO WHAT NAV LINKS DO
