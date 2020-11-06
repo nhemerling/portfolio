ce6bec0364b885342ec4ea3ed960068dc1407c96
Nicholas Hemerling <nicholas.hemerling@gmail.com>
2020-11-05 21:14:34 -0800

    update portfolio with Burnt Toast and Considerate, need to add LangTrain

--- src/About/About.js
+++ src/About/About.js
@@ -7,17 +7,17 @@ export default function About(props) {
             <h2>About Me</h2>
             <div className="group">
                 <div className="item">
-                    <p>My favorite thing about coding is solving logical problems. 
-                        I have always loved the rush of adrenaline I get when trying to solve puzzles. 
+                    <p>My favorite thing about coding is solving logical problems.
+                        I have always loved the rush of adrenaline I get when trying to solve puzzles.
                         Coding gives me that rush and I'd love to work on a team with great programmers who feel the same.</p>
-                    <p>I also have many years of experience in theater as an actor and comedian, 
-                        and love bringing my sense of humor and story-telling to the projects I work on, 
-                        as well as the spaces I work in. Reach out if you like to have fun while building 
+                    <p>I also have many years of experience in theater as an actor and comedian,
+                        and love bringing my sense of humor and story-telling to the projects I work on,
+                        as well as the spaces I work in. Reach out if you like to have fun while building
                         your website!</p>
 
                     <h3>Services I Provide</h3>
                     <ul>
-                        <li>JS, HTML, CSS</li>
+                        <li>JS, HTML, CSS, React, Node, Express, SQL</li>
                         <li>Beautiful, mobile responsive design</li>
                         <li>Great communication with my clients</li>
                         <li>Finding a sense of humor for your project</li>
--- src/Portfolio/Portfolio.js
+++ src/Portfolio/Portfolio.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import nicoPortfolio from '../images/nicoBeardGlasses.jpg';
+import ConsiderateHomePage from '../images/ConsiderateHomePage.png';
+import BurntToastLandingPage from '../images/BurntToastLandingPage.png';
 
 export default function Portfolio(props) {
     return(
@@ -8,21 +10,24 @@ export default function Portfolio(props) {
             <div className="group">
                 <div className="item">
                     <a href="/" target="_blank" rel="noopener noreferrer">
-                        <img src={nicoPortfolio} alt="Nicholas Hemerling with a bear and glasses" className="rounded" />
+                        <img src={ConsiderateHomePage} alt="Considerate App Home Page" className="rounded" />
                     </a>
-                    <h3>Smell Good App for the Bad Smelling</h3>
-                    <p>Do you have trouble smelling good? Would you like to be better at smelling good?</p>
-                    <p>I designed this app to help you smell good. I used my smell good skills to make it.</p>
-                    <p>Check it out <a href="/" target="_blank" rel="noopener noreferrer">here</a>.</p>
+                    <h3>Considerate</h3>
+                    <p>Considerate helps you keep track of the things your friends like and of the occasions when you'd like to give them the perfect gift.</p>
+                    <p>I used React.js and CSS on the front-end, and Node.js, Express, and PostgreSQL on the back-end.</p>
+                    <p>Check out the app <a href="https://considerate-app.vercel.app" target="_blank" rel="noopener noreferrer">here</a>.</p>
+                    <p>And the GitHub repo <a href="https://github.com/nhemerling/considerate-client" target="_blank" rel="noopener noreferrer">here</a>.</p>
                 </div>
                 <div className="item">
                     <a href="/" target="_blank" rel="noopener noreferrer">
-                        <img src={nicoPortfolio} alt="Nicholas Hemerling with a bear and glasses" className="rounded" />
+                        <img src={BurntToastLandingPage} alt="Burnt Toast App LAnding Page" className="rounded" />
                     </a>
-                    <h3>Website for the Governor of the Universe</h3>
-                    <p>Got a call one day. I was asked to make a website. It took me a long time but I'm really pleased with how it turned out. The Governor loves it too.</p>
-                    <p>I built this site to fix the universe for The Governor. I used JS, HTML, and CSS to build it.</p>
-                    <p>Check it out <a href="/" target="_blank" rel="noopener noreferrer">here</a>.</p>
+                    <h3>Burnt Toast</h3>
+                    <p>Burnt Toast helps users barter services with each other instead of trading currency.</p>
+                    <p>We help them provide and seek services, and get connected.</p>
+                    <p>We used React.js and CSS on the front-end, and Node.js, Express, and PostgreSQL on the back-end.</p>
+                    <p>Check the app <a href="https://burnt-toast-app.vercel.app" target="_blank" rel="noopener noreferrer">here</a>.</p>
+                    <p>And the GitHub repo <a href="https://github.com/thinkful-ei-quail/burnt-toast-client" target="_blank" rel="noopener noreferrer">here</a>.</p>
                 </div>
                 <div className="item">
                     <a href="/" target="_blank" rel="noopener noreferrer">
@@ -31,7 +36,7 @@ export default function Portfolio(props) {
                     <h3>Secret Cybersecurity Project</h3>
                     <p>The less said about this project, the better. If you are NASA, FBI, CIA, or Elon Musk, please stop bothering me.</p>
                     <p>I started this project to help my mom with her emails. I created it utilizing the power of the Oxford Comma, and PHP.</p>
-                    <p>Check it out <a href="/" target="_blank" rel="noopener noreferrer">here</a>.</p>
+                    <p>Check out the app <a href="/" target="_blank" rel="noopener noreferrer">here</a>.</p>
                 </div>
             </div>
         </section>
