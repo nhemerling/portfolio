3ea3affdc4c6e953332532f2b40388cf2cce7f71
Nicholas Hemerling <nicholas.hemerling@gmail.com>
2020-11-06 10:39:34 -0800

    add LangTrain to portfolio

--- src/Portfolio/Portfolio.js
+++ src/Portfolio/Portfolio.js
@@ -2,6 +2,7 @@ import React from 'react';
 import nicoPortfolio from '../images/nicoBeardGlasses.jpg';
 import ConsiderateHomePage from '../images/ConsiderateHomePage.png';
 import BurntToastLandingPage from '../images/BurntToastLandingPage.png';
+import LangTrainLogin from '../images/LangTrainLogin.png';
 
 export default function Portfolio(props) {
     return(
@@ -9,7 +10,7 @@ export default function Portfolio(props) {
             <h2>Portfolio</h2>
             <div className="group">
                 <div className="item">
-                    <a href="/" target="_blank" rel="noopener noreferrer">
+                    <a href="https://considerate-app.vercel.app" target="_blank" rel="noopener noreferrer">
                         <img src={ConsiderateHomePage} alt="Considerate App Home Page" className="rounded" />
                     </a>
                     <h3>Considerate</h3>
@@ -19,7 +20,7 @@ export default function Portfolio(props) {
                     <p>And the GitHub repo <a href="https://github.com/nhemerling/considerate-client" target="_blank" rel="noopener noreferrer">here</a>.</p>
                 </div>
                 <div className="item">
-                    <a href="/" target="_blank" rel="noopener noreferrer">
+                    <a href="https://burnt-toast-app.vercel.app" target="_blank" rel="noopener noreferrer">
                         <img src={BurntToastLandingPage} alt="Burnt Toast App LAnding Page" className="rounded" />
                     </a>
                     <h3>Burnt Toast</h3>
@@ -30,13 +31,14 @@ export default function Portfolio(props) {
                     <p>And the GitHub repo <a href="https://github.com/thinkful-ei-quail/burnt-toast-client" target="_blank" rel="noopener noreferrer">here</a>.</p>
                 </div>
                 <div className="item">
-                    <a href="/" target="_blank" rel="noopener noreferrer">
-                        <img src={nicoPortfolio} alt="Nicholas Hemerling with a bear and glasses" className="rounded" />
+                    <a href="https://lang-train.vercel.app/" target="_blank" rel="noopener noreferrer">
+                        <img src={LangTrainLogin} alt="LangTrain App Login Page" className="rounded" />
                     </a>
-                    <h3>Secret Cybersecurity Project</h3>
-                    <p>The less said about this project, the better. If you are NASA, FBI, CIA, or Elon Musk, please stop bothering me.</p>
-                    <p>I started this project to help my mom with her emails. I created it utilizing the power of the Oxford Comma, and PHP.</p>
-                    <p>Check out the app <a href="/" target="_blank" rel="noopener noreferrer">here</a>.</p>
+                    <h3>LangTrain</h3>
+                    <p>Lang Train is an app that uses spaced repetition to teach the user a few words in German.</p>
+                    <p>We used React.js and CSS on the front-end, and Node.js, Express, and PostgreSQL on the back-end.</p>
+                    <p>Check out the app <a href="https://lang-train.vercel.app/" target="_blank" rel="noopener noreferrer">here</a>.</p>
+                    <p>And the GitHub repo <a href="https://github.com/thinkful-ei-quail/Spaced-Repetition-App-NicholasH-Jim" target="_blank" rel="noopener noreferrer">here</a>.</p>
                 </div>
             </div>
         </section>
